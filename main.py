ae9c0fb0d5d1e56be462fafd1095f2f3ef255040
Joel Bryla <JBryla@rbc.local>
2018-02-21 16:26:57 +1100

    added multiple cams, changed format and added time

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ import time
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--refresh_rate', '-r', default=0.1, type=float, help="Time interval between each data point in seconds")
-parser.add_argument("--directory", "--dir", "-f", type=str, help="Directory to save log files")
+parser.add_argument('--refresh_rate', '--rate', "-r", default=20, type=float, help="Time interval between each data point in seconds")
+parser.add_argument("--directory", "--dir", "-f", type=str, default="", help="Directory to save log files")
+parser.add_argument("--server", "--ip", type=str, default="roborio-4774-frc.local", help="ip of the roborio")
+parser.add_argument("--camera", "--cam", "-c", action='append', type=str, help="ip of camera server")
 
 args = parser.parse_args()
-server = "roborio-4774-frc.local"
 
-if args.directory is None:
-    args.directory = "replays/testing/"
+if args.camera is None:
+	args.camera = ["http://10.47.74.36:1181/stream.mjpg", "http://10.47.74.36:1182/stream.mjpg"]  # [0,1] testing
 
-main_camera = Recorder(args.directory + "main_cam.avi", "http://10.47.74.36:1181/stream.mjpg", 1 / args.refresh_rate)  # 1 / args.refresh_rate)
-vision_camera = Recorder(args.directory + "vision_cam.avi", "http://10.47.74.36:1182/stream.mjpg", 1 / args.refresh_rate)
+current_time = time.strftime("%H%M %d-%m-%Y")
+streams = [Recorder(args.directory +"cam" + str(num) +" "+ current_time + ".mp4", stream, int(max(args.refresh_rate,1))) for num, stream in enumerate(args.camera, start=1)]	
 
 if __name__ == "__main__":
     while True:
         start = time.monotonic()
 
-        main_camera.record()
-        vision_camera.record()
+        for stream in streams:
+        	stream.record()
 
         elapsed = time.monotonic() - start
-        if args.refresh_rate - elapsed > 0:
-            time.sleep(args.refresh_rate - elapsed)
+        if 1/args.refresh_rate - elapsed > 0:
+            time.sleep(1/args.refresh_rate - elapsed)
--- record.py
+++ record.py
@@ -3,10 +3,11 @@ import cv2
 
 class Recorder:
     def __init__(self, output, camera_url, fps):
+        self.file = output
         self.stream = cv2.VideoCapture(camera_url)
         width = self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)
         height = self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        self.recorder = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*'XVID'), fps, (int(width), int(height)))
+        self.recorder = cv2.VideoWriter(self.file, cv2.VideoWriter_fourcc(*'MP4V'), fps, (int(width), int(height)))
 
     def record(self):
         ret, frame = self.stream.read()
